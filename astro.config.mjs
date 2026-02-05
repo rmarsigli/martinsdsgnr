7859fb64c3883fbe5910ec06c7810866a43043b7
Rafhael Marsigli <rmarsigli@gmail.com>
2026-02-05 10:36:41 -0300

    feat: configure Tailwind v4 and unplugin-icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
+import Icons from 'unplugin-icons/vite'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://martinsdsgnr.github.io',
+	vite: {
+		plugins: [
+			tailwindcss(),
+			Icons({
+				compiler: 'astro',
+			}),
+		],
+	},
+})
