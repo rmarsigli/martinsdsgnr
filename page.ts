b67e03a8c7e550c5c4b309b10808bc91d4e0b5cd
Rafhael Marsigli <rmarsigli@gmail.com>
2026-02-05 10:36:47 -0300

    assets: update images and favicon

--- page.ts
+++
@@ -1,9 +0,0 @@
-export const data = {
-    title: "Vinicius Martins",
-    slogan: "Seu slogan aqui",
-    description: "Sua Description aqui",
-    links: {
-        behance: 'https://www.behance.net/martinsdsgnr',
-        whatsapp: 'https://wa.me/5561999999999',
-    },
-}
